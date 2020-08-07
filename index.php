8c05a0ed785de145e08dcf70930d87db126cbb39
mxdr <max@7u3.ru>
2020-08-07 21:18:16 +0300

    Main index.php file for testing execution.

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once './vendor/autoload.php';
+
+use LeadGenerator\Generator;
+
+$generator = new Generator();
+$generator->generateLeads(10000, '\maxodrom\leadgentest\Handler::processLead');
